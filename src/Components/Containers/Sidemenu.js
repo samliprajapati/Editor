e56510ef11aed168e77a146c198b28711c170002
samliprajapati <samliprajapati9@gmail.com>
2020-02-17 21:14:33 +0530

    react-draft-wysiwyg package added

--- src/Components/Containers/Sidemenu.js
+++ src/Components/Containers/Sidemenu.js
@@ -14,19 +14,28 @@ export default class Sidemenu extends Component {
         Name: "Collection1.1",
         Description:
           "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-        Heading: "WYSIWYG EDITOR"
+        Heading: "WYSIWYG EDITOR",
+        subHeading: "Topic name",
+        subDescription:
+          "The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from de Finibus Bonorum et Malorum by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham"
       },
       {
         Name: "Collection1.1.1",
         Description:
           "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).",
-        Heading: "WYSIWYG EDITOR"
+        Heading: "WYSIWYG EDITOR",
+        subHeading: "Topic name",
+        subDescription:
+          "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable."
       },
       {
         Name: "Collection1.2",
         Description:
           "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
-        Heading: "WYSIWYG EDITOR"
+        Heading: "WYSIWYG EDITOR",
+        subHeading: "Topic name",
+        subDescription:
+          "All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable."
       }
     ],
     currentContent: ""
@@ -71,6 +80,13 @@ export default class Sidemenu extends Component {
             </div>
             <div className="content-description">
               {this.state.currentContent.Description}
+            </div>
+            <div className="content-heading">
+              <hr />
+              {this.state.currentContent.subHeading}
+            </div>
+            <div className="content-description">
+              {this.state.currentContent.subDescription}
             </div>
           </Content>
         </Layout>
